f52c2ac936975dd3f51e38fe75cda35aa9cc907c
Papa Smurf <chris@violet.co>
2023-02-24 11:24:13 +0000

    test: added tests for mintBatch

--- test/violetID/VID.behavior.ts
+++ test/violetID/VID.behavior.ts
@@ -62,4 +62,27 @@ export function shouldBehaveLikeVioletID(): void {
       );
     });
   });
+
+  describe("mintBatch", async function () {
+    it("as admin should succeed", async function () {
+      await expect(this.violetID.connect(this.signers.admin).mintBatch(this.signers.user.address, [0], [1], "0x00")).to
+        .not.be.reverted;
+    });
+
+    it("as owner should fail", async function () {
+      await expect(
+        this.violetID.connect(this.signers.owner).mintBatch(this.signers.user.address, [0], [1], "0x00"),
+      ).to.be.revertedWith(
+        `AccessControl: account ${this.signers.owner.address.toLowerCase()} is missing role ${ADMIN_ROLE}`,
+      );
+    });
+
+    it("as user should fail", async function () {
+      await expect(
+        this.violetID.connect(this.signers.user).mintBatch(this.signers.user.address, [0], [1], "0x00"),
+      ).to.be.revertedWith(
+        `AccessControl: account ${this.signers.user.address.toLowerCase()} is missing role ${ADMIN_ROLE}`,
+      );
+    });
+  });
 }
