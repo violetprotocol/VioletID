96efffc6b4f49aee1a44c042940822d8a6f639d9
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-06-28 11:12:39 +0100

    feat: more comment

--- tasks/deploy/violetID.ts
+++ tasks/deploy/violetID.ts
@@ -3,7 +3,8 @@ import type { TaskArguments } from "hardhat/types";
 
 import { VioletID, VioletID__factory } from "../../src/types";
 
-// 💡 Make sure to delete /artifacts and /cache and then run `yarn compile` before running this task.
+// 💡 Make sure to delete /artifacts and /cache and then run `yarn compile` before running this task,
+// and that you have specified the right Etherscan API key in .env.
 task("deploy:VioletID").setAction(async function (_taskArguments: TaskArguments, { ethers, upgrades, run }) {
   console.log(`Deploying Violet ID...`);
 
