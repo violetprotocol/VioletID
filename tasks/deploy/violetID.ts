e15aca1b9823c9cbab77fb02bb3b1ceb46455188
Raphael <10075759+ra-phael@users.noreply.github.com>
2023-07-03 09:17:36 +0100

    [DADZ-287] add verification step when deploying (#14)
    * feat: add verification step when deploying
    
    * feat: more comment

--- tasks/deploy/violetID.ts
+++ tasks/deploy/violetID.ts
@@ -3,11 +3,21 @@ import type { TaskArguments } from "hardhat/types";
 
 import { VioletID, VioletID__factory } from "../../src/types";
 
-task("deploy:VioletID").setAction(async function (taskArguments: TaskArguments, { ethers, upgrades }) {
+// 💡 Make sure to delete /artifacts and /cache and then run `yarn compile` before running this task,
+// and that you have specified the right Etherscan API key in .env.
+task("deploy:VioletID").setAction(async function (_taskArguments: TaskArguments, { ethers, upgrades, run }) {
+  console.log(`Deploying Violet ID...`);
+
   const VioletIDFactory: VioletID__factory = <VioletID__factory>await ethers.getContractFactory("VioletID");
   const violetID: VioletID = <VioletID>await upgrades.deployProxy(VioletIDFactory, [], { initializer: "initialize" });
   await violetID.deployed();
   console.log(`VioletID deployed at: ${violetID.address}`);
+
+  console.log("Verifying contract...");
+
+  await run("verify:verify", {
+    address: violetID.address,
+  });
 });
 
 export default {};
