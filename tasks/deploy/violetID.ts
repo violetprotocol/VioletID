54024fb73ac3bde3cb895ee9b9ed4ce80b486ed5
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-07-04 13:42:02 +0100

    feat: better logs

--- tasks/deploy/violetID.ts
+++ tasks/deploy/violetID.ts
@@ -6,18 +6,22 @@ import { VioletID, VioletID__factory } from "../../src/types";
 // 💡 Make sure to delete /artifacts and /cache and then run `yarn compile` before running this task,
 // and that you have specified the right Etherscan API key in .env.
 task("deploy:VioletID").setAction(async function (_taskArguments: TaskArguments, { ethers, upgrades, run }) {
-  console.log(`Deploying Violet ID...`);
+  const signer = ethers.provider.getSigner();
+  const signerAddress = await signer.getAddress();
+  console.log(`Deploying Violet ID with signer ${signerAddress}.`);
 
   const VioletIDFactory: VioletID__factory = <VioletID__factory>await ethers.getContractFactory("VioletID");
   const violetID: VioletID = <VioletID>await upgrades.deployProxy(VioletIDFactory, [], { initializer: "initialize" });
   await violetID.deployed();
-  console.log(`VioletID deployed at: ${violetID.address}`);
+  console.log(`🚀 VioletID deployed at: ${violetID.address}`);
 
   console.log("Verifying contract...");
 
   await run("verify:verify", {
     address: violetID.address,
   });
+
+  console.log("Task complete ✅");
 });
 
 export default {};
