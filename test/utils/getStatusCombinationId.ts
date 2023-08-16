88c63ac1001eb565a4cd9ee9119a6d0bbf2dee46
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-08-16 17:29:29 +0100

    feat: rename util to get status combination id

--- test/utils/getStatusCombinationId.ts
+++ test/utils/getStatusCombinationId.ts
@@ -1,4 +1,4 @@
-export const getAttributeCombinationId = (attributeIds: number[]) =>
+export const getStatusCombinationId = (attributeIds: number[]) =>
   attributeIds.reduce((acc, attributeIndex) => {
     return acc | (1 << attributeIndex);
   }, 0);
