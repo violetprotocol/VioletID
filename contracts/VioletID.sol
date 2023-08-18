35c9a8179151fdd5ef3131167727a18d2954030a
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-08-18 11:57:57 +0100

    fix: rename file

--- contracts/VioletID.sol
+++ contracts/VioletID.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 import "./IVioletID.sol";
-import "./StatusMapByAddress.sol";
+import "./StatusMap.sol";
 import "./temp/AccessTokenConsumerUpgradeable.sol";
 
 contract VioletID is
