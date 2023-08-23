e7ce457f45dfb7d8f966a74139bbe84635a34f1f
Papa Smurf <chris@violet.co>
2023-08-23 13:44:52 +0200

    chore: solhint

--- contracts/VioletID.sol
+++ contracts/VioletID.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-import "./IVioletID.sol";
-import "./StatusMap.sol";
-import "./temp/AccessTokenConsumerUpgradeable.sol";
+import { IVioletID } from "./IVioletID.sol";
+import { StatusMap } from "./StatusMap.sol";
+import { AccessTokenConsumerUpgradeable } from "./temp/AccessTokenConsumerUpgradeable.sol";
 
 /**
  * @dev VioletID contract
