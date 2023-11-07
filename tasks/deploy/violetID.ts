279a1370f4bd317cf9cd48f463a10915879a5f7b
Raphael <10075759+ra-phael@users.noreply.github.com>
2023-11-07 12:18:59 +0000

    Deployments (#27)
    * feat: update task to deploy with eat verifier
    
    * feat: add deployments artifacts
    
    * feat: update readme

--- tasks/deploy/violetID.ts
+++ tasks/deploy/violetID.ts
@@ -5,23 +5,31 @@ import { VioletID, VioletID__factory } from "../../src/types";
 
 // 💡 Make sure to delete /artifacts and /cache and then run `yarn compile` before running this task,
 // and that you have specified the right Etherscan API key in .env.
-task("deploy:VioletID").setAction(async function (_taskArguments: TaskArguments, { ethers, upgrades, run }) {
-  const signer = ethers.provider.getSigner();
-  const signerAddress = await signer.getAddress();
-  console.log(`Deploying Violet ID with signer ${signerAddress}.`);
+task("deploy:VioletID")
+  .addParam("eatverifier", "Contract address of VioletID contract")
+  .setAction(async function (_taskArguments: TaskArguments, { ethers, upgrades, run }) {
+    const signer = ethers.provider.getSigner();
+    const signerAddress = await signer.getAddress();
+    console.log(`Deploying Violet ID with signer ${signerAddress}.`);
 
-  const VioletIDFactory: VioletID__factory = <VioletID__factory>await ethers.getContractFactory("VioletID");
-  const violetID: VioletID = <VioletID>await upgrades.deployProxy(VioletIDFactory, [], { initializer: "initialize" });
-  await violetID.deployTransaction.wait(5);
-  console.log(`🚀 VioletID deployed at: ${violetID.address}`);
+    const eatVerifier = _taskArguments.eatverifier;
 
-  console.log("Verifying contract...");
+    if (!eatVerifier) throw new Error("Missing EAT verifier address. Make sure it is passed as --eatverifier param");
 
-  await run("verify:verify", {
-    address: violetID.address,
+    const VioletIDFactory: VioletID__factory = <VioletID__factory>await ethers.getContractFactory("VioletID");
+    const violetID: VioletID = <VioletID>(
+      await upgrades.deployProxy(VioletIDFactory, [eatVerifier], { initializer: "initialize" })
+    );
+    await violetID.deployTransaction.wait(5);
+    console.log(`🚀 VioletID deployed at: ${violetID.address}`);
+
+    console.log("Verifying contract...");
+
+    await run("verify:verify", {
+      address: violetID.address,
+    });
+
+    console.log("Task complete ✅");
   });
-
-  console.log("Task complete ✅");
-});
 
 export default {};
