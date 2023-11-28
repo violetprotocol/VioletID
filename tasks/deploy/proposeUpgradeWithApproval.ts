76c56a25d0820617d0218c510fec4c5bdcede265
Gabriel Ferraz <sudoferraz@gmail.com>
2023-11-28 11:44:09 +0200

    chore: proposeUpgradeWithApproval task

--- tasks/deploy/proposeUpgradeWithApproval.ts
+++ tasks/deploy/proposeUpgradeWithApproval.ts
@@ -1,13 +1,16 @@
 import { task } from "hardhat/config";
 import type { TaskArguments } from "hardhat/types";
 
-task("proposeUpgradeWithApproval").setAction(async function (_taskArguments: TaskArguments, { ethers, defender, artifacts }) {
-  const newVIDContract = await ethers.getContractFactoryFromArtifact(await artifacts.readArtifact("VioletIDv1_4_0"));
+task("proposeUpgradeWithApproval").setAction(async function (_taskArguments: TaskArguments, { defender }) {
+  // const newVIDContract = await ethers.getContractFactory("VioletIDv1_4_0");
   try {
     const response = await defender.proposeUpgradeWithApproval(
       // Sepolia previous VID contract
       "0xE62C5A18Fb0e5f9b01bE9fAc679040e142e0e1FC",
-      newVIDContract,
+      "VioletIDv1_4_0",
+      {
+        approvalProcessId: "95f8f653-1112-419a-ba23-bbc22dd5120a"
+      }
     );
     console.log(response);
   } catch (error) {
